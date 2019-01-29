ae32bcd6f4dd97282154166b4f41d99e349f149f
Dustin <dustinwilson@dustins-macbook-air.local>
2019-01-28 20:13:48 -0600

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Dustin Wilson on 1/28/19.
+//  Copyright © 2019 Dustin Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
